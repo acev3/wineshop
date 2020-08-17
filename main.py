2db3cb53d3d04cbd38b54156d257d9f2797176a6
acev3 <kovalchukilya5@gmail.com>
2020-08-17 18:40:18 +0300

    Refactoring template
    Ref

--- main.py
+++ main.py
@@ -13,20 +13,16 @@ now = datetime.datetime.now()
 now_year = now.year
 start_year = 1920
 life_year = now_year - start_year
-
-template = env.get_template('template.html')
-rendered_page = template.render(
-    life_year = str(life_year)
-)
 excel_data_df = pandas.read_excel('wine_example.xlsx', usecols=['Категория', 'Название', 'Сорт', 'Цена', 'Картинка', 'Акция'],
                                   na_values='', keep_default_na=False)
 excel_data_df  = excel_data_df.fillna('')
 products_group = collections.defaultdict(list)
 for element in excel_data_df.to_dict(orient='record'):
     products_group[element['Категория']].append(element)
-    element.pop('Категория')
 
-rendered_page = template.render(categories=products_group)
+template = env.get_template('template.html')
+rendered_page = template.render(categories=products_group,
+                                life_year = str(life_year))
 
 with open('index.html', 'w', encoding="utf8") as file:
     file.write(rendered_page)
